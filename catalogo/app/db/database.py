1ec9ae51d6ce81c51e916efc1204b32cb5f1d6cf
DanielQ <mosi1995@gmail.com>
2021-01-11 18:35:22 +0000

    myapp para Cloud run.

---
+++ catalogo/app/db/database.py
@@ -0,0 +1,44 @@
+import psycopg2
+from flask import g
+
+def get_db():
+
+    db_user = os.environ.get('DB_USERNAME')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_name = os.environ.get('DB_NAME')
+    db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME', None)
+
+    if 'db' not in g and not db_connection_name:
+        # If there is no db_connection_name we assume that we are connecting
+        # using a TCP socket, and therefore we need a HOST and a PORT to
+        # connect to.
+        db_host = os.environ.get('DB_HOST')
+        db_port = os.environ.get('DB_PORT')
+        g.db = psycopg2.connect(
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+            database=db_name
+        )
+
+    if 'db' not in g and db_connection_name:
+        # If there is a db_connection_name then we assume that we are
+        # connecting to a unix socket, whose name is google-specific.
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        g.db = psycopg2.connect(
+            user=db_user,
+            password=db_password,
+            host=unix_socket,
+            database=db_name
+        )
+
+    return g.db
+
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
---
+++ catalogo/app/main.py
@@ -0,0 +1,77 @@
+from flask import jsonify, request, Flask
+from catalog import get_products, create_product, get_product
+from flask_cors import CORS
+import os
+import redis
+
+
+app = Flask(__name__)
+CORS(app)
+
+# redis_client = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), db=0, decode_responses=True)
+
+redis_host = os.environ.get('REDIS_HOST', None)
+redis_port = os.environ.get('REDIS_PORT', None)
+
+if redis_host and redis_port:
+    redis_client = redis.Redis(
+        host=os.getenv('REDIS_HOST'),
+        port=os.getenv('REDIS_PORT'),
+        db=0, decode_responses=True
+    )
+else:
+    redis_client = None
+
+@app.route('/product/<sku>', methods=['GET', ])
+def get_product_by_sku(sku):
+#	product = redis_client.hgetall(sku)
+	product = redis_client.hgetall(sku) if redis_client else None
+	if not product:
+		product = get_product(sku)
+		product['cache'] = 'miss'
+#		redis_client.hmset(product['sku'], product)
+		if redic_client:
+			redis_client.hmset(product['sku'], product)
+	else:
+		pass
+		product['cache'] = 'hit'
+
+	return jsonify(product)
+
+
+@app.route('/product', methods=['GET', 'POST'])
+def list_all_products():
+	'''This view manages the CRUD of products'''
+	print("Hola mundo")
+	if request.method == 'GET':
+		response = get_products()
+		return jsonify(response)
+	
+	if request.method == 'POST':
+		data = request.get_json()
+		new_sku = create_product(
+			None,
+			data['title'],
+			data['long_description'],
+			data['price_euro'])
+		return jsonify({"status": "ok", "sku": new_sku})
+
+
+@app.route('/hello')
+def hello_world():
+	message = "Hola Mundo, soy Python! Ahora con CloudBuild y hablando JSON"
+	response = {
+		"message": message,
+		"length": len(message)
+	}
+	return jsonify(response)
+
+
+@app.route('/bye')
+def bye_world():
+	return ("Adios mundo cruel")
+
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
+
